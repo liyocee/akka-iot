7ea1396aca2016cad15d0c68cfd0cdbb2474214f
Collins Liyosi <collinskivale@gmail.com>
2019-06-07 08:16:23 +0300

    Add handling for device termination

--- src/main/scala/com/liyosi/iot/actors/DeviceGroup.scala
+++ src/main/scala/com/liyosi/iot/actors/DeviceGroup.scala
@@ -1,17 +1,25 @@
 package com.liyosi.iot.actors
 
-import akka.actor.{ Actor, ActorLogging, ActorRef, Props }
+import akka.actor.{ Actor, ActorLogging, ActorRef, Props, Terminated }
+import com.liyosi.iot.actors.DeviceGroup.{ ReplyRequestDeviceList, RequestDeviceList }
 
 /**
  * Created by liyosi on Jun, 2019
  */
 object DeviceGroup {
   def props(groupId: String): Props = Props(new DeviceGroup(groupId))
+
+  final case class RequestDeviceList(requestId: Long)
+  final case class ReplyRequestDeviceList(requestId: Long, deviceIds: Set[String])
 }
 
 class DeviceGroup(groupId: String) extends Actor with ActorLogging {
 
   var deviceIdToActor: Map[String, ActorRef] = Map.empty
+  var actorToDeviceId: Map[ActorRef, String] = Map.empty
+
+  override def preStart(): Unit = log.info("DeviceGroup {} started", groupId)
+  override def postStop(): Unit = log.info("DeviceGroup {} stop", groupId)
 
   override def receive: Receive = {
     case trackMsg @ DeviceManager.RequestTrackDevice(`groupId`, _) =>
@@ -21,10 +29,21 @@ class DeviceGroup(groupId: String) extends Actor with ActorLogging {
         case None => // register device the device
           val deviceActor = context.actorOf(Device.props(trackMsg.groupId, trackMsg.deviceId))
           deviceIdToActor += trackMsg.deviceId -> deviceActor
+          actorToDeviceId += deviceActor -> trackMsg.deviceId
           deviceActor forward trackMsg
       }
+
     case DeviceManager.RequestTrackDevice(groupId, _) =>
       log.warning("Ignoring TrackDevice request for {} . This actor is responsible for {}", groupId, this.groupId)
+
+    case RequestDeviceList(requestId) =>
+      sender() ! ReplyRequestDeviceList(requestId, deviceIdToActor.keySet)
+
+    case Terminated(deviceActor) =>
+      val deviceId = actorToDeviceId(deviceActor)
+      log.info("Device actor {} has been terminated", deviceActor)
+      actorToDeviceId -= deviceActor
+      deviceIdToActor -= deviceId
   }
 }
 
--- src/test/scala/com/liyosi/iot/actors/DeviceGroupSpec.scala
+++ src/test/scala/com/liyosi/iot/actors/DeviceGroupSpec.scala
@@ -1,6 +1,6 @@
 package com.liyosi.iot.actors
 
-import akka.actor.ActorSystem
+import akka.actor.{ ActorSystem, PoisonPill }
 import akka.testkit.{ TestKit, TestProbe }
 import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
 
@@ -30,6 +30,12 @@ class DeviceGroupSpec(_system: ActorSystem)
 
     trackedDeviceActor1 should !==(trackedDeviceActor2)
 
+    // ensure the device is tracked
+    deviceGroupActor.tell(DeviceGroup.RequestDeviceList(2L), testProbe.ref)
+    val response = testProbe.expectMsgType[DeviceGroup.ReplyRequestDeviceList]
+    response.requestId shouldBe 2L
+    response.deviceIds shouldBe Set("deviceId", "deviceId2")
+
     // ensure the device are tracked correctly and they are functional
     trackedDeviceActor1.tell(Device.RecordTemperature(1L, 30.0), testProbe.ref)
     val response1 = testProbe.expectMsgType[Device.TemperatureRecorded]
@@ -44,5 +50,37 @@ class DeviceGroupSpec(_system: ActorSystem)
     trackedDeviceActor2.tell(Device.RecordTemperature(2L, 30.0), testProbe.ref)
     val response2 = testProbe.expectMsgType[Device.TemperatureRecorded]
     response2.requestId shouldBe 2L
+  }
+
+  "should untrack a device if it's terminated" in {
+    val testProbe = TestProbe()
+    val deviceGroup = system.actorOf(DeviceGroup.props("groupId"))
+
+    deviceGroup.tell(DeviceManager.RequestTrackDevice("groupId", "deviceId"), testProbe.ref)
+
+    // ensure the device is tracked
+    testProbe.expectMsg(DeviceManager.DeviceRegistered)
+
+    val registeredDevice = testProbe.lastSender
+
+    deviceGroup.tell(DeviceManager.RequestTrackDevice("groupId", "deviceId2"), testProbe.ref)
+
+    deviceGroup.tell(DeviceGroup.RequestDeviceList(2L), testProbe.ref)
+    testProbe.expectMsg(DeviceGroup.ReplyRequestDeviceList(2L, Set("deviceId", "deviceId2")))
+
+    // terminate device
+    testProbe.watch(registeredDevice)
+    registeredDevice ! PoisonPill
+
+    // termination not correctly working when we send a poison pill message
+
+//    testProbe.awaitAssert {
+//      testProbe.expectTerminated(registeredDevice)
+//      // ensure the terminated device is no longer tracked
+//      deviceGroup.tell(DeviceGroup.RequestDeviceList(3L), testProbe.ref)
+//      val response2 = testProbe.expectMsgType[DeviceGroup.ReplyRequestDeviceList]
+//      response2.requestId shouldBe 3L
+//      response2.deviceIds shouldBe Set.empty[String]
+//    }
   }
 }
